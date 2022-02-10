62594e07f0b14c6ddffd74e16b07736980cb4fff
pritom48 <pritamdas41148@gmail.com>
2022-02-10 18:38:57 +0600

    banking added

---
+++ js/banking.js
@@ -0,0 +1,12 @@
+document.getElementById('deposit-button').addEventListener('click', function () {
+    const depositInput = document.getElementById('deposit-input');
+    const depositAmount = depositInput.value;
+    console.log(depositAmount);
+
+
+    const depositTotal = document.getElementById('deposit-total');
+    depositTotal.innerText = depositAmount;
+
+    // clear the deposit input field
+    depositInput.value = "";
+})
