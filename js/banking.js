2852a545eece9c8428013a60e73300d80bfe0807
pritom48 <pritamdas41148@gmail.com>
2022-02-11 17:14:16 +0600

    deposit added

--- js/banking.js
+++ js/banking.js
@@ -1,11 +1,21 @@
 document.getElementById('deposit-button').addEventListener('click', function () {
     const depositInput = document.getElementById('deposit-input');
-    const depositAmount = depositInput.value;
-    console.log(depositAmount);
+    const depositAmountText = depositInput.value;
+    const depositAmount = parseFloat(depositAmountText);
+    // console.log(depositAmount);
 
-
+    //get currunt deposit
     const depositTotal = document.getElementById('deposit-total');
-    depositTotal.innerText = depositAmount;
+    const depositTotalText = depositTotal.innerText;
+    const depositTotalNumber = parseFloat(depositTotalText);
+    depositTotal.innerText = depositTotalNumber + depositAmount;
+
+    // update total balance
+    const totalBalance = document.getElementById('total-balance');
+    const totalBalanceText = totalBalance.innerText;
+    const totalbalanceAmount = parseFloat(totalBalanceText);
+    totalBalance.innerText = totalbalanceAmount + depositAmount;
+
 
     // clear the deposit input field
     depositInput.value = "";
